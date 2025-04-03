2d67a9d69a87ae3d6fca560e6abdb8a8751125f2
Kalpesh Patel <Kalpesh Patel@DESKTOP-QHQFNE8>
2025-04-03 21:42:14 +0530

    Add project files.

---
+++ DynamicTimetable/Controllers/TimetableController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using DynamicTimetable.Models;
+
+namespace DynamicTimetable.Controllers
+{
+    public class TimetableController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult _SubjectInputs(int model)
+        //{
+        //    return PartialView(model);
+        //}
+
+        [HttpPost]
+        public IActionResult _SubjectInputs(TimetableRequest model)
+        {
+
+            return PartialView("_SubjectInputs", model);
+        }
+
+
+        [HttpPost]
+        public JsonResult GenerateTimetable([FromBody] TimetableRequest request)
+        {
+            if (request == null || request.WorkingDays <= 0 || request.SubjectsPerDay <= 0 || request.Subjects == null)
+            {
+                return Json(new { error = "Invalid input data." });
+            }
+
+            int workingDays = request.WorkingDays;
+            int subjectsPerDay = request.SubjectsPerDay;
+            List<SubjectModel> subjects = request.Subjects;
+
+            int totalHours = workingDays * subjectsPerDay;
+            List<List<string>> timetable = new List<List<string>>();
+
+            
+            for (int i = 0; i < subjectsPerDay; i++)
+            {
+                timetable.Add(new List<string>(new string[workingDays]));
+            }
+
+            int row = 0, col = 0;
+
+            
+            foreach (var subject in subjects)
+            {
+                int remainingHours = subject.Hours;
+
+                while (remainingHours > 0)
+                {
+                    if (row >= subjectsPerDay) row = 0;
+                    if (col >= workingDays) col = 0;
+
+                    if (string.IsNullOrEmpty(timetable[row][col]))
+                    {
+                        timetable[row][col] = subject.SubjectName;
+                        remainingHours--;
+
+                        col++;
+                        if (col >= workingDays)
+                        {
+                            col = 0;
+                            row++;
+                        }
+                    }
+                    else
+                    {
+                        col++;
+                        if (col >= workingDays)
+                        {
+                            col = 0;
+                            row++;
+                        }
+                    }
+                }
+            }
+
+            return Json(timetable);
+        }
+    }
+
+
+}
---
+++ DynamicTimetable/Models/TimetableModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicTimetable.Models
+{
+    public class SubjectModel
+    {
+        public string SubjectName { get; set; }
+        public int Hours { get; set; }
+        public int TotalHoursForWeek { get; set; }
+    }
+
+    public class TimetableRequest
+    {
+        public int WorkingDays { get; set; }
+        public int SubjectsPerDay { get; set; }
+        public int TotalSubjects { get; set; }
+        public int TotalHoursForWeek { get; set; }
+        public List<SubjectModel> Subjects { get; set; }
+
+        public string SubjectName { get; set; }
+        public int Hours { get; set; }
+    }
+}
